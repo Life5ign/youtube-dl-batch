36c4b1947999c3de32830bf9d9ab2163fb8d6ae5
bryant <brz73001@gmail.com>
2020-09-14 14:33:43 -0600

    Define the project root directory in vars.sh
    This is done using git rev-parse.  Refactor other variables to use this
    directory instead of one that changed based on the location of the
    script that sourced vars.sh for more predictable behavior, especially
    when defining the absolute paths of project directories (e.g. config)

--- vars.sh
+++ vars.sh
@@ -1,12 +1,18 @@
 # file containing variables to be sourced
 
-# define the directory containing the sourcing script
+# define the project root directory, which is determined by the output of a git
+# rev-parse command
+PROJECT_ROOT_DIR="$(git rev-parse --show-toplevel)"
+
+# define the directory of any script that sources this file.  NOTE: this will
+# change based on the directory that this script is located in.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# define the basename of this script
+# define the basename of the script that sources this file, without removing
+# any suffix
 BASENAME="$(basename $0)"
 
-# define the absolute path of this script
+# define the absolute path of the script that sources this file
 SCRIPTPATH="${DIR}/${BASENAME}"
 
 # the name of the sourcing script, without a .sh suffix; useful for creating
@@ -14,7 +20,7 @@ SCRIPTPATH="${DIR}/${BASENAME}"
 BASENAME_S="$(basename -s ".sh" $0)"
 
 # the configuration directory's absolute path
-CONFIG_DIR="${DIR}/config"
+CONFIG_DIR="${PROJECT_ROOT_DIR}/config"
 
 # define the path to the base config file, which is applied to all
 # downloads 
@@ -26,16 +32,16 @@ CONFIG_BASENAME="config"
 BATCH_BASENAME="batch"
 
 # the tmp directory's absolute path
-TMP_DIR="${DIR}/tmp"
+TMP_DIR="${PROJECT_ROOT_DIR}/tmp"
 
 # the batch directory's absolute path
-BATCH_DIR="${DIR}/batch"
+BATCH_DIR="${PROJECT_ROOT_DIR}/batch"
 
 # the script directory's absolute path
-SCRIPT_DIR="${DIR}/scripts"
+SCRIPT_DIR="${PROJECT_ROOT_DIR}/scripts"
 
 # the location where media will be downloaded
 DOWNLOAD_DIR="${HOME}/Music/youtube-dl/batch"
 
 # define archive file
-ARCHIVE_FILE="${DIR}/archive"
+ARCHIVE_FILE="${PROJECT_ROOT_DIR}/archive"
