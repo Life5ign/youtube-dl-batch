54a2be58c197f6e4071c5140fa05ee514251280f
bryant <brz73001@gmail.com>
2020-09-21 15:11:50 -0600

    Remove unused scripts and add a gitignore to scripts

--- scripts/youtube-dl-test-sleep.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#start youtube-dl, and have it sleep for 4 hours before downloading a file, for testing reasons
-youtube-dl --sleep-interval 14400 https://www.youtube.com/watch?v=rni__JjSxk0
